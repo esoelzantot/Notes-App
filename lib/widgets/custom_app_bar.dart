f31f50eeab05eab684c124535c82c50176214ce1
EsoElzantot <eslamelzantot@gmail.com>
2025-08-28 17:23:52 +0300

    feat(ui): create custom app bar component replacing default ActionBar

---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+AppBar buildAppBar() {
+  return AppBar(
+    elevation: 0,
+    title: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+      child: const Text(
+        "Notes",
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.w400),
+      ),
+    ),
+    actions: [
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: IconButton(
+          style: IconButton.styleFrom(
+            backgroundColor: Colors.grey.withAlpha(50),
+            iconSize: 30,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+          ),
+          onPressed: () {},
+          icon: const Icon(Icons.search, color: Colors.white),
+        ),
+      ),
+    ],
+  );
+}
